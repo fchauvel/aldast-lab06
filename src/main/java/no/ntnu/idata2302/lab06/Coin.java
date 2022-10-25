fd465c8a257ea9288e564bfc2e9c0680d7c3de3a
Franck Chauvel <fchauvel@Francks-MacBook-Air.local>
2022-10-25 06:28:42 +0200

    bootstrap: Dummy file to get the Java project started

---
+++ src/main/java/no/ntnu/idata2302/lab06/Coin.java
@@ -0,0 +1,10 @@
+package no.ntnu.idata2302.lab06;
+
+
+public class Coin {
+
+    public static void main(String args[]) {
+        System.out.println("Hi guys!");
+    }
+
+}
---
+++ src/test/java/no/ntnu/idata2302/lab06/CoinTest.java
@@ -0,0 +1,14 @@
+package no.ntnu.idata2302.lab06;
+
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class CoinTest {
+
+    @Test
+    public void sampleTest() {
+        assertEquals(4, 2 + 2);
+    }
+
+}
